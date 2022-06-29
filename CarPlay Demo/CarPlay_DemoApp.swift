72f0711700ef0f2cfc9a3f48edf144dc21957d82
Bipin Butala <bipinbipin@gmail.com>
2022-06-29 10:53:48 -0500

    Initial Commit

---
+++ CarPlay Demo/CarPlay_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarPlay_DemoApp.swift
+//  CarPlay Demo
+//
+//  Created by Bipin Butala on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CarPlay_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarPlay Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarPlay Demo
+//
+//  Created by Bipin Butala on 6/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
